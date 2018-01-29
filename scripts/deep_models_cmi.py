9a52f146391314e6d63b9c464d8e2af4b54ef5b8
Admin <administrator@playground.hortonworks>
2018-01-29 15:15:29 +0100

    Changes - Running program

--- scripts/deep_models_cmi.py
+++ scripts/deep_models_cmi.py
@@ -4,19 +4,18 @@ from keras.layers import Conv2D, MaxPooling2D
 from keras.applications.vgg16 import VGG16
 from keras.applications.inception_v3 import InceptionV3
 from keras.applications.densenet import DenseNet121
-
 from keras.layers import Activation, Dropout, Flatten, Dense
 from keras.preprocessing.image import ImageDataGenerator
 from keras.regularizers import l2
 from keras.layers.normalization import BatchNormalization
 from keras import optimizers
 from keras.models import Model
+import glob
 import os
 from keras.callbacks import ModelCheckpoint
 import cv2
 import numpy as np
 import pandas as pd
-import warnings
 
 print(keras.__version__)
 
@@ -119,6 +118,49 @@ def get_custom_model():
     return model, "custom"
 
 
+def create_generators(batch_size):
+    # Generators
+    datagen = ImageDataGenerator(
+        rescale=1. / 255, rotation_range=10,
+        # width_shift_range=0.2, height_shift_range=0.2,
+        # rescale=1./255,
+        # shear_range=0.2,
+        # zoom_range=0.2,
+        horizontal_flip=True, vertical_flip=True
+    )
+
+    val_datagen = ImageDataGenerator(
+        rescale=1. / 255, horizontal_flip=True,
+        vertical_flip=True
+    )
+
+    test_datagen = ImageDataGenerator(
+        rescale=1. / 255
+    )
+
+    train_generator = datagen.flow_from_directory(
+        train_set,
+        target_size=(512, 512),
+        batch_size=batch_size,
+        class_mode='categorical'
+    )
+
+    validation_generator = val_datagen.flow_from_directory(
+        val_set,
+        target_size=(512, 512),
+        batch_size=batch_size,
+        class_mode='categorical'
+    )
+
+    test_generator = test_datagen.flow_from_directory(
+        test_set,
+        target_size=(512, 512),
+        batch_size=1,
+        save_format="tif",
+        class_mode='categorical'
+    )
+    return train_generator, validation_generator, test_generator
+
 ########
 # Paths#
 ########
@@ -132,10 +174,20 @@ test_set = "../processed_data/test_folder"
 models = os.listdir(train_set)
 print(models)
 
+
+########
+# Data #
+########
+
+# Define batch size
+batch_size = 8
+train_generator, validation_generator, test_generator = create_generators(batch_size)
+
 #########
 # Model #
 #########
 
+# Define model !!!
 # options: {get_densNet121, get_vgg16, get_inceptionv3, get_custom_model}
 model, model_name = get_densnet121()
 
@@ -143,7 +195,8 @@ model, model_name = get_densnet121()
 # Loss, optimizer and metrics! #
 ################################
 
-# Optimizer
+# Optimizer !!!
+
 #adam = optimizers.Adam(lr=0.0001)
 sgd = optimizers.sgd(lr=0.0001)
 
@@ -153,3 +206,52 @@ model.compile(loss = 'categorical_crossentropy',
                 optimizer = sgd,
                 metrics = ['accuracy'])
 
+# save model
+directory = "weights/" + model_name
+if not os.path.exists(directory):
+            os.makedirs(directory)
+# Checkpoint
+filepath = directory + "/weights-improvement-{epoch:02d}-{val_acc:.2f}.hdf5"
+checkpoint = ModelCheckpoint(filepath, monitor='val_acc', verbose=1, save_best_only=True, mode='max')
+callbacks_list = [checkpoint]
+
+# Fit the model
+model.fit_generator(train_generator,
+        steps_per_epoch= 100, #100, #30
+        epochs= 10,  #20
+        callbacks=callbacks_list,
+        validation_data=validation_generator,
+        validation_steps=20)
+
+
+############################
+# Predictions & Submission #
+############################
+
+#  get last written file (best val_acc)
+list_of_weights = glob.glob(directory + "/*")
+filepath = max(list_of_weights, key=os.path.getctime)
+
+# load the weights of the best trained model
+model.load_weights(filepath)
+test_filenames = test_generator.filenames
+classes_dict = train_generator.class_indices
+predictions = model.predict_generator(test_generator, len(test_filenames))
+# Get predictions
+test_files = []
+final_predictions = []
+
+for i, pred in enumerate(predictions):
+     test_files.append(test_filenames[i].split("/")[-1])
+     final_predictions.append(get_key_by_value(classes_dict, np.argmax(pred)))
+
+# Dump predictions in csv
+submission_dir = "submission"
+# # Write Submission File
+df = pd.DataFrame(columns=['fname', 'camera'])
+df['fname'] = test_files
+df['camera'] = final_predictions
+sub_name = model_name + "_" + filepath.split("/")[-1] + ".csv"
+sub_file = os.path.join(submission_dir, sub_name)
+df.to_csv(sub_file, index=False)
+print("DONE!")
